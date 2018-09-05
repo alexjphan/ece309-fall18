8c969fdeac70fd6e38176ed02971c3371a04eb52
alexjphan <43013653+alexjphan@users.noreply.github.com>
2018-09-05 16:49:34 -0400

    Update hw2.cpp

--- hw2.cpp
+++ hw2.cpp
@@ -1,18 +1,44 @@
 #include <stdio.h>
+#include <string>
+using namespace std
+  
 class List {
   public:
-    List();
+    List()
+        {
+       // there’s nothing in the list
+       head = NULL;
+       tail = NULL;
+        }
     length();
     get();
-    push_back();
+    void push_back(string a)
+    {
+      append(a);
+    }
     remove_front();
-    List::~List()
+    ~List()
     {
       // Free all of the ListNodes in the list
       Item t;
       while(!empty()) // while not empty
          remove(t);   // remove the next node
     }
+      void append(string a) 
+      {
+      ListNode *node = new ListNode(a);
+      if (head==NULL) { 
+         // list is empty, so set head and 
+         // tail to be node
+         head = node;
+         tail = node;
+      } else {
+        // put new node at end of list
+        tail->setNext(node); //(1)
+        tail = node;         //(2)
+      }
+      }
+   
     bool remove(Item &copy) 
     {
         if (!empty()) // if list is not empty
@@ -27,6 +53,23 @@ class List {
         }
     return false; // nothing in list
     }
+  private:
+    ListNode *head;
+    ListNode *tail;
   
+};
+  
+  class ListNode {
+private:
+   string word; // data in the list
+   ListNode *next;	
+public:
+   ListNode(string a) 
+      { word = a; next=NULL; }
+   ListNode* getNext() { return next; }
+   void setNext(ListNode *n) { next = n; }
+   string getWord() { return word; }
+};
+
   
 
