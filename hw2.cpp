e81d7461699a5879e528832caded882e10ee7921
alexjphan <43013653+alexjphan@users.noreply.github.com>
2018-09-05 16:32:53 -0400

    Update hw2.cpp

--- hw2.cpp
+++ hw2.cpp
@@ -1,5 +1,32 @@
 #include <stdio.h>
 class List {
   public:
-  List();
-  length();
+    List();
+    length();
+    get();
+    push_back();
+    remove_front();
+    List::~List()
+    {
+      // Free all of the ListNodes in the list
+      Item t;
+      while(!empty()) // while not empty
+         remove(t);   // remove the next node
+    }
+    bool remove(Item &copy) 
+    {
+        if (!empty()) // if list is not empty
+        {
+           copy = head->getItem(); // return copy
+            ListNode *tmp = head->getNext();
+            delete head; // delete the node
+            head = tmp;  // update the head
+            if (tmp==NULL) // removed last element 
+               tail = NULL;
+            return true;
+        }
+    return false; // nothing in list
+    }
+  
+  
+
