793f0032063bcd0a929c540eba1d475cd3761899
alexjphan <43013653+alexjphan@users.noreply.github.com>
2018-09-27 01:39:12 -0400

    finished

--- lib/PList.cpp
+++ lib/PList.cpp
@@ -52,28 +52,4 @@ void DList::erase(DList::iterator it)
 
 }
 
-int main()
-{
-  DList l;
 
-  Num *two = new Num(2);
-  Doub *pi = new Doub(3.14);
-  Strn *yo = new Strn("yo");
-
-  l.append(two);
-  l.append(pi);
-  l.append(yo);
-
-
-  DList::iterator i;
-  printf("Forward: ");
-  for(i = l.begin();!i.end();i.increment())
-  {
-    Object *object = i.getObject();
-    object->print();
-  }
-
-  printf("\n");
-
-  return 0;
-}
