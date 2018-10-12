741375d05ebef2fb5015bd14830ef28b5cf31af8
alexjphan <43013653+alexjphan@users.noreply.github.com>
2018-10-12 00:26:50 -0400

    bruh omg

---
+++ tests/IntegerSetArray.h
@@ -0,0 +1,47 @@
+class IntegerSet {
+protected:
+   int size;
+   int hash(int key) { return (key * 997) % size; }
+public:
+   IntegerSet(int htsize):size(htsize) {}
+   virtual bool insert(int) = 0;
+   virtual bool search(int) const = 0;
+   virtual void remove(int) = 0;
+};
+
+class IntegerSetArray:public IntegerSet {
+protected:
+    int *a;
+public:
+    IntegerSetArray(int arraysize):IntegerSet(arraysize) {
+        a = new int[arraysize];
+        for(int i = 0; i < arraysize; i++) {
+            a[i] = -1;
+        }
+    }
+    bool insert(int x) {
+        for (int i = 0; i < IntegerSet::size; i++) {
+            if (a[i] < 0) {
+                a[i] = x;
+                return true;
+            }
+        }
+        return false;
+    }
+    bool search(int x) const {
+        for (int i = 0; i < IntegerSet::size; i++) {
+            if (a[i] == x) {
+                return true;
+            }
+        }
+        return false;
+    }
+    void remove(int x){
+        for (int i = 0; i < IntegerSet::size; i++) {
+            if (a[i] == x) {
+                a[i] = -1;
+            }
+        }
+    }
+
+};
